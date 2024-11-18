85d4798661a5ec6de489f0773edd5479deea8580
Fabian885-cmd <fabiancg790@gmail.com>
2024-11-18 03:09:14 -0300

    segundo commit

--- tarifa.js
+++ tarifa.js
@@ -9,4 +9,5 @@ else{
     console.log("Debido a que su consumo es menor a 300KWH mantiene un subsidio del 15%")
 }*/
 
-(consumoKWH > 300) ? (pagoMes *= 1.2, console.log(`Debido a que su hogar tuvo un consumo de ${consumoKWH}kWh, en base al ajuste tarifario (hogares con consumo mayor a 300kWh por mes tendrán un aumento del 20%), cumplimos con informarle que se ha ajustado el total a pagar, que será de ${pagoMes}.`)) : console.log("Debido a que su consumo es menor a 300KWH mantiene un subsidio del 15%") ;
+(consumoKWH > 300) ? (pagoMes *= 1.2, console.log(`Debido a que su hogar tuvo un consumo de ${consumoKWH}kWh, en base al ajuste tarifario (hogares con consumo mayor a 300kWh por mes tendrán un aumento del 20%), cumplimos con informarle que se ha ajustado el total a pagar, que será de ${pagoMes}.`))
+: console.log("Debido a que su consumo es menor a 300KWH mantiene un subsidio del 15%") ;
